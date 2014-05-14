56394ad1340b5badfc7bd5d2e0895f8e19dbf544
Alex Hawkins <alexhawkins.me@gmail.com>
2014-05-14 13:55:29 -0700

    create STAKIT app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stakit::Application.config.secret_key_base = 'f541d3319335f08748256747909da9ea9b40362bbcf3a310119607c1d654ae0092231e8dae363905810da39f873555e611c7a726459a29fdf853fceb31a6a703'
