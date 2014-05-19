92539a8c6b4f240cf5dab3da891c27358e14baf1
Alex Hawkins <alexhawkins.me@gmail.com>
2014-05-19 10:07:47 -0700

    Implement in-place editing features for X-editable

--- app/models/frequency.rb
+++ app/models/frequency.rb
@@ -1,4 +1,12 @@
 class Frequency < ActiveRecord::Base
   has_many :stack_supplements, dependent: :destroy
   #has_many :supplements, through: :stack_supplements
+
+  def self.to_xeditable
+    ret = {}
+    Frequency.all.each do |freq| 
+      ret["#{freq.id}"] = "#{freq.value}"
+    end
+    ret.to_json
+  end
 end
