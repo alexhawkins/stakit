775a165a8ccc77b0382f1ada9476b6195b991530
Alex Hawkins <alexhawkins.me@gmail.com>
2014-06-09 20:44:07 -0700

    Fix question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -42,7 +42,7 @@ class Question < ActiveRecord::Base
 
   def self.text_search(query)
     if query.present?
-     #query = query.split(" ").join(" & ") 
+     query = query.split(" ").join(" & ") 
      search(query)
     else
      order('created_at desc')
