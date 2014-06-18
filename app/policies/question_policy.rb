2b829cbe756b939e1cc6de10f71649aa9169099e
Alex Hawkins <alexhawkins.me@gmail.com>
2014-06-18 16:09:08 -0700

    Update edit policy

--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -1,9 +1,2 @@
 class QuestionPolicy < ApplicationPolicy
-
-  def edit?
-    # A user needs to be either the record's owner or the general administrator,
-    # on top of being present and logged-in:
-    user.present?
-  end
-
 end
