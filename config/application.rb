071fe8377ef8b043bb6faeed036a08676f24f348
Alex Hawkins <alexhawkins.me@gmail.com>
2014-06-18 16:53:12 -0700

    Fix

--- config/application.rb
+++ config/application.rb
@@ -27,22 +27,5 @@ module Stakit
     #config.middleware.insert_before 0, "SupplementSuggestions"
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
-
-    # config/application.rb
-    config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
-      else
-        false
-      end
-    end
   end
 end
