c7bdda35581c7d5242bdc337da7b3c3dde6a6f51
Alex Hawkins <alexhawkins.me@gmail.com>
2014-06-19 15:24:53 -0700

    Add Favorites and Favorite count to Stacks and Questions

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,6 +3,7 @@ class FavoritesController < ApplicationController
   def create
     @stack = Stack.find(params[:stack_id])
     favorite = current_user.favorites.build(stack: @stack)
+    authorize favorite
     if favorite.save
       flash[:notice] = "Favorited Stack"
       redirect_to :back
@@ -15,6 +16,7 @@ class FavoritesController < ApplicationController
   def destroy
     stack = Stack.find(params[:stack_id])
     favorite = current_user.favorites.find(params[:id])
+    authorize favorite
     if favorite.destroy
       flash[:notice] = "Removed favorite."
       redirect_to :back
